d450c51a8a50b88158bb510641997371e3419636
Vinod <11861588+vsramalwan@users.noreply.github.com>
2024-10-05 21:07:59 +0200

    chore: update ckad-bashrc.sh

--- scripts/ckad-bashrc.sh
+++ scripts/ckad-bashrc.sh
@@ -1,6 +1,10 @@
 # courtesy: https://gist.github.com/JamieMac96/adf9d3c9fe9aa6cd40a20047efabc9ec
 alias k="kubectl"
 alias v="vim"
+alias kgp="kubectl get pod"
+alias kgd="kubectl get deploy"
+alias kgs="kubectl get svc"
+alias kgn="kubectl get nodes"
 
 function ns () {
   kubectl config set-context --current --namespace=$1
