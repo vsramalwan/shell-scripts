46db40add6532104b828e2d0893132312f5e2b11
Vinod <vsramalwan@gmail.com>
2024-10-13 12:48:30 +0200

    chore: update install kubeadm script

--- scripts/install-kubeadm-debian.sh
+++ scripts/install-kubeadm-debian.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+KUBERNETES_VERSION=v1.31
 
-apt-get update && apt-get upgrade
-apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo apt-get update && apt-get upgrade
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBERNETES_VERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBERNETES_VERSION}/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+echo "Installing kubelet, kubeadm and kubectl..."
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
+echo "Enabling kubelet service..."
 systemctl enable --now kubelet
